6d79c1b1a755866fe861c0c311c58df002c4a5b3
nicolas <nicolassosamachado25@gmail.com>
2023-11-28 23:06:55 -0300

    começando a api

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,85 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+  async signup(dto: AuthDto) {
+    // gerar a senha
+    const hash = await argon.hash(dto.password);
+
+    try {
+      //salva o usuario
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      delete user.hash;
+
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code == 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
+  }
+
+  async signin(dto: AuthDto) {
+    // achar o usuario pelo email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+    // se não existe retornar um erro
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    //verificar senha
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '30m',
+      secret: secret,
+    });
+
+    return {
+      access_token: token,
+    };
+  }
+}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { getUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/guard';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  //Pode ser passado direto   @UseGuards(AuthGuard('jwt')), mas criar um custom guard evita erros.
+  //O parametro pode ser passado assim:@Req() req: Request e acessar req.user
+  //porém podemos criar um custom decorator para facilitar
+  //a leitura
+
+  constructor(private userService: UserService) {}
+
+  @Get('me')
+  getMe(@getUser() user: User, @getUser('email') email: string) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@getUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.userService.editUser(userId, dto);
+  }
+}
