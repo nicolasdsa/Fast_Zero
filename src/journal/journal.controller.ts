a11ab6a5fdaaeba1e00e7400283e144e8acb8662
nicolas <nicolassosamachado25@gmail.com>
2024-01-09 19:51:43 -0300

    mudança na rota

--- src/journal/journal.controller.ts
+++ src/journal/journal.controller.ts
@@ -2,7 +2,7 @@ import { JournalService } from './journal.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { createJournalDto } from './dto';
 
-@Controller('journal')
+@Controller('journals')
 export class JournalController {
   constructor(private journalService: JournalService) {}
 
