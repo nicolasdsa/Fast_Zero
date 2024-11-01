45b120dda5b3df3f73af58148bf410b07d95a1bf
nicolas <nicolassosamachado25@gmail.com>
2024-11-01 00:47:26 -0300

    Aula 09 + Exercicios finalizados

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,24 +2,42 @@ from dataclasses import asdict
 
 from sqlalchemy import select
 
-from fast_zero.models import User
+from fast_zero.models import Todo, User
 
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
         new_user = User(
-            username='alice', password='secret', email='teste@test'
+            username='nicolas', password='teste', email='teste@gmail.com'
         )
         session.add(new_user)
         session.commit()
 
-        user = session.scalar(select(User).where(User.username == 'alice'))
+        user = session.scalar(select(User).where(User.username == 'nicolas'))
 
     assert asdict(user) == {
         'id': 1,
-        'username': 'alice',
-        'password': 'secret',
-        'email': 'teste@test',
+        'username': 'nicolas',
+        'password': 'teste',
+        'email': 'teste@gmail.com',
+        'todos': [],
         'created_at': time,
         'updated_at': time,
     }
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Descrição',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
